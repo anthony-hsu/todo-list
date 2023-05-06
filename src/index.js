0371aa1f1ed18f59fbf194e24060b4b80090c9e3
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-05-06 15:26:34 -0500

    Make styling changes

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import './style.css';
 import Project from './project';
 import TodoItem from './todoItem';
 
-const project_list = [];
+const projectList = [];
 
 // Create a new project
 const a = new Project("Project 1");
-project_list.push(a);
+projectList.push(a);
 
 // Create a new TodoItem
 const todo = new TodoItem("Study", "Do some leetcode", "5/5/2023", 3);
 a.addTodoItem(todo);
 
-// Do something with project_list (i.e. print all todoItems to log)
-project_list.forEach(p => {
+// Do something with projectList (i.e. print all todoItems to log)
+const divContainer = document.getElementById('container');
+const projectTitle = document.createElement('h1');
+
+const currentProject = projectList[0];
+projectTitle.innerHTML = currentProject.title;
+divContainer.appendChild(projectTitle);
+
+projectList.forEach(p => {
     (p.todoItems).forEach(element => {
         console.log(element);
     })
