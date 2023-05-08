a02fb78933567a9005d98665e8a048bf76451060
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-05-08 09:00:04 -0500

    Add new project form as modal and overlay

--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
 import './style.css';
 import Project from './project';
 import TodoItem from './todoItem';
+import loadProject from './loadProject';
 
 const projectList = [];
 
 // Create a new project
+// TODO: When new project is created, add an event listener to it, which calls "loadProject()"
 const a = new Project("Project 1");
+const b = new Project("Project 2");
 projectList.push(a);
+projectList.push(b);
 
 // Create a new TodoItem
-const todo = new TodoItem("Study", "Do some leetcode", "5/5/2023", 3);
-a.addTodoItem(todo);
+const todo1 = new TodoItem("Study", "Do some leetcode", "5/5/2023", 3);
+const todo2 = new TodoItem("Eat", "Eat some food", "5/5/2023", 2);
+a.addTodoItem(todo1);
+a.addTodoItem(todo2);
+const todo3 = new TodoItem("Run", "Run a mile", "5/5/2023", 1);
+const todo4 = new TodoItem("Sleep", "Get some sleep", "5/5/2023", 5);
+b.addTodoItem(todo3);
+b.addTodoItem(todo4);
 
 // Do something with projectList (i.e. print all todoItems to log)
-const divContainer = document.getElementById('container');
-const projectTitle = document.createElement('h1');
-
-const currentProject = projectList[0];
-projectTitle.innerHTML = currentProject.title;
-divContainer.appendChild(projectTitle);
-
-projectList.forEach(p => {
-    (p.todoItems).forEach(element => {
-        console.log(element);
-    })
+const projectDropdown = document.getElementById('dropdown-project-list');
+projectList.forEach(project => {
+    const item = document.createElement('a');
+    item.href = "#";
+    item.innerHTML = project.title;
+    item.addEventListener('click', () => {
+        // Load project
+        loadProject(project);
+    });
+    projectDropdown.appendChild(item);
 });
+
+// Initialize New dropdowns
+const newProjectMenu = document.getElementById('menu-new-project');
+newProjectMenu.addEventListener('click', () => {
+    const newProjectContainer = document.getElementById('new-project-container');
+    const overlay = document.getElementById('overlay');
+    newProjectContainer.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
+});
+
+loadProject(a);
---
+++ src/loadProject.js
@@ -0,0 +1,36 @@
+const loadProject = (project) => {
+    const divContainer = document.getElementById('container');
+    divContainer.replaceChildren();
+
+    const projectTitle = document.createElement('h1');
+    projectTitle.innerHTML = project.title;
+    divContainer.appendChild(projectTitle);
+
+    // append div of todoList "cards" -- divContainer.appendChild(divTodoList);
+    (project.todoItems).forEach(todoItem => {
+        const divTodoList = document.createElement('div');
+        const todoTitle = document.createElement('h3');
+        const todoDescription = document.createElement('p');
+        const todoDueDate = document.createElement('p');
+        const todoPriority = document.createElement('p');
+
+        // Title
+        todoTitle.innerHTML = todoItem.title;
+        divTodoList.appendChild(todoTitle);
+        // Description
+        todoDescription.innerHTML = `Description: ${todoItem.description}`;
+        divTodoList.appendChild(todoDescription);
+        // DueDate
+        todoDueDate.innerHTML = `Due Date: ${todoItem.dueDate}`;
+        divTodoList.appendChild(todoDueDate);
+        // Priority
+        todoPriority.innerHTML = `Priority: ${todoItem.priority}`;
+        divTodoList.appendChild(todoPriority);
+
+        divTodoList.classList.add("todoItem");
+        divContainer.appendChild(divTodoList);
+        divContainer.appendChild(document.createElement("br"));
+    });
+}
+
+export default loadProject;
